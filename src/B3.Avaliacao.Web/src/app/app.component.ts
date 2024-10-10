031d7a9145dc88d53843887d2e8b82077db18284
fernandocrmeneses <fernandocrm_2@outlook.com>
2024-10-10 15:47:09 -0300

    camada de front e back end finalizada.

--- src/B3.Avaliacao.Web/src/app/app.component.ts
+++ src/B3.Avaliacao.Web/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormBuilder, FormControlName, Form } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { ComponentService } from './component.service';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,16 +16,16 @@ import { Subject } from 'rxjs';
        <p>Valor Bruto:<span id="valorBruto"></span></p>
        <p>Valor Líquido: <span id="valorLiquido"></span></p>
 
-      <button (click)="obtemValores(mes.value,valor.value)">Enviar</button>
+      <button (click)="ObtemValores(mes.value,valor.value)">Enviar</button>
   `
 })
 
 export class AppComponent {
    constructor(private componentService: ComponentService) { }
 
-   obtemValores(mes: string, valor: string) {
+   ObtemValores(mes: string, valor: string) {
 
-   this.ValidacaoCampos(mes, valor);
+     if (!this.ValidacaoCampos(mes, valor)) return;
 
    this.componentService.calculaInvestmento(parseFloat(valor), parseInt(mes))
     .subscribe({
@@ -44,24 +42,28 @@ export class AppComponent {
     });
   }
 
-  ValidacaoCampos(mes: string, valor: string) {
+  ValidacaoCampos(mes: string, valor: string) : boolean {
 
     if (!Number(mes) || !Number(valor)) {
       alert("Mês e valor monetario em um formato incorreto! Digitar somente numeros.");
-      return;
+      return false;
     }
 
     if (parseInt(mes) > 12) {
       alert("Valores digitados incorretamente! Digite de 1 a 12 no campo mês.");
-      return;
+      return false;
     }
 
     if (parseInt(mes) <= 0) {
       alert("Valores digitados incorretamente! O campo mês deve ser maior ou igual a 1.");
+      return false;
     }
 
     if (parseInt(valor) <= 0) {
       alert("Valores digitados incorretamente! O campo valor monetário deve ser maior que zero.");
+      return false;
     }
+
+    return true;
   }
 }
